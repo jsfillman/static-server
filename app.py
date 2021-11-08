0b9794d31170d1bb5aa28cc2ab9f7cf262a65dc4
Jason S Fillman <6155956+jsfillman@users.noreply.github.com>
2021-11-08 12:40:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from os import path
 
 git_url = "https://github.com/kbase/static-content.git"
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/static')
 
 # This route serves static/index.html at the route /
-@app.route("/")
+@app.route('/')
 def index():
   if path.exists('./static'):
     return app.send_static_file('index.html')
@@ -17,7 +17,7 @@ def index():
     return app.send_static_file('index.html')
 
 
-@app.route("/pull")
+@app.route('/pull/')
 def pull():
   if path.exists('./static'):
     d = git.cmd.Git('static')
